005a6bd23753d8e3937a94de92d2c062590e2abe
adityamahamiya <aditya.mahamiya@outlook.com>
2021-06-07 04:12:23 +0530

    String oprations

--- hello-world.py
+++ hello-world.py
@@ -1,11 +1,4 @@
 # stings
-my_string = "My name is Jhon"
-print(my_string[0])
-my_string = "My name is Jhon"
-print(my_string[-1])
-
-print(len(my_string))
-print(my_string.lower())
-print(my_string.upper())
-print(my_string.replace('Jhon','Tom'))
-print(my_string.count('is'))
+fruit = "I like apples, mangoes, bananas"
+print(fruit)
+print(fruit.split(' '))
