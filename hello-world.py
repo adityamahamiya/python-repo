78e9235ffbf4b0300e16bfa87b23d27fb3cc8001
adityamahamiya <aditya.mahamiya@outlook.com>
2021-06-07 04:08:58 +0530

    String oprations

--- hello-world.py
+++ hello-world.py
@@ -1,26 +1,11 @@
-# ARETMATIC OPPRATIONS
-num1 = 10
-num2 = 20
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
-# RELATIONAL OPERATIONS
-print(num1 < num2)
-print(num1 > num2)
-print(num1 == num2)
-print(num1 != num2)
-# LOGIACL OPERATIONS
-log1 = True
-log2 = False
-# &
-print(log1 & log2)
-print(log2 & log1)
-print(log2 & log2)
-print(log1 & log1)
-# |
+# stings
+my_string = "My name is Jhon"
+print(my_string[0])
+my_string = "My name is Jhon"
+print(my_string[-1])
 
-print(log1 | log2)
-print(log2 | log1)
-print(log2 | log2)
-print(log1 | log1)
+print(len(my_string))
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.replace('Jhon','Tom'))
+print(my_string.count('is'))
