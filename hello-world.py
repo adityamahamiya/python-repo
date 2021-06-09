3a99c75e0c083c05aa779532f7990e1c84d1aef5
adityamahamiya <aditya.mahamiya@outlook.com>
2021-06-09 22:50:47 +0530

    Joining NumPy

--- hello-world.py
+++ hello-world.py
@@ -423,16 +423,78 @@ print(gc.show_gender())
 print()
 # NumPy (SINGLE DIMENSIONAL ARRAY)
 import numpy as np
+
 n1 = np.array([10, 20, 30, 40])
 print(n1)
 print()
 print(type(n1))
 print()
 import numpy as np
-n2=np.array([[10, 20, 30, 40], [40, 30, 20, 10]])
+
+n2 = np.array([[10, 20, 30, 40], [40, 30, 20, 10]])
 print(n2)
 print()
+# numpy with zeros
+import numpy as np
 
+n1 = np.zeros((1, 2))
+print(n1)
+print()
+import numpy as np
 
+n1 = np.zeros((5, 5))
+print(n1)
+print()
+# NumPy with same number
+import numpy as np
 
+n1 = np.full((2, 2), 10)
+print(n1)
+print()
+# NumPy with arrange
+import numpy as np
+
+n1 = np.arange(10, 20)
+print(n1)
+print()
+import numpy as np
+
+n1 = np.arange(10, 50, 5)
+print(n1)
+print()
+# NumPy with random numbers
+import numpy as np
+
+n1 = np.random.randint(1, 100, 5)
+print(n1)
+print()
+# NumPy with shapes
+import numpy as np
+
+n1 = np.array([[1, 2, 3], [4, 5, 6]])
+print(n1.shape)
+print()
+n1.shape = (3, 2)
+print(n1)
+print()
+# Joining NumPy
+import numpy as np
+
+n1 = np.array([10, 20, 30])
+n2 = np.array([40, 50, 60])
+print(np.vstack((n1, n2)))
+print()
+print(np.hstack((n1, n2)))
+print()
+print(np.column_stack((n1, n2)))
+print()
+import numpy as np
+
+n1 = np.array([10, 20, 30, 40, 50])
+n2 = np.array([40, 50, 60, 70, 80])
+print(np.intersect1d(n1, n2))
+print()
+print(np.setdiff1d(n1, n2))
+print()
+print(np.setdiff1d(n2, n1))
 
