dac3c8748c36dc12e506c835fcb0ce0a64352bf1
adityamahamiya <aditya.mahamiya@outlook.com>
2021-06-10 16:18:36 +0530

    table

---
+++ table.py
@@ -0,0 +1,9 @@
+for i in range(1, 13):
+    print(i, end="\t")
+print()
+print("---------------------------------------------------------------------------------\n")
+
+for i in range(1, 11):
+    for n in range(1, 11):
+        print(i * n, end="\t")
+print("\n")
